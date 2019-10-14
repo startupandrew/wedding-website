6568567eeaa988ad46658658ac90801c91fce480
startupandrew <startupandrew@gmail.com>
2019-10-13 19:43:31 -0700

    spacing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "30px 0"
                 });
                 $('header .member-actions').css({
                     "top": "26px",
@@ -109,7 +109,7 @@ $(document).ready(function () {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
+                    "padding": "38px 0"
                 });
                 $('header .member-actions').css({
                     "top": "41px",
